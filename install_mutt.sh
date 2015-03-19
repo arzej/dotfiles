cafd27b9650ec07ef261beeb05bd27e040365739
Andrzej Kapluk <akapluk@inverto.tv>
2015-03-19 16:17:54 +0100

    additional mutt stuff

--- install_mutt.sh
+++ install_mutt.sh
@@ -3,6 +3,12 @@ if [ -d "$DIRECTORY" ]; then
     rm -rf $DIRECTORY
 fi
 if [ ! -d "$DIRECTORY" ]; then
+    wget -q -O - ftp://ftp.mutt.org/mutt/devel/mutt-1.5.23.tar.gz | tar xvfz -
+    cd ./mutt-1.5.23
+    wget -q -O - http://lunar-linux.org/~tchan/mutt/patch-1.5.23.sidebar.20140412.txt | patch -p1
+    ./configure --enable-imap --enable-smtp --enable-pop --enable-mailtool  --with-ssl
+    make
+    sudo make install
     mkdir ~/.mutt
     mkdir ~/.mutt/themes/
     git clone https://github.com/altercation/mutt-colors-solarized.git
