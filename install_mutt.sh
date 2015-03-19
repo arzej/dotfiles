5f170a6250f4712e4f4aa5b37eca54eaa07027b2
Andrzej Kapluk <akapluk@inverto.tv>
2015-03-19 17:02:40 +0100

    enable email cache and html view

--- install_mutt.sh
+++ install_mutt.sh
@@ -6,7 +6,7 @@ if [ ! -d "$DIRECTORY" ]; then
     wget -q -O - ftp://ftp.mutt.org/mutt/devel/mutt-1.5.23.tar.gz | tar xvfz -
     cd ./mutt-1.5.23
     wget -q -O - http://lunar-linux.org/~tchan/mutt/patch-1.5.23.sidebar.20140412.txt | patch -p1
-    ./configure --enable-imap --enable-smtp --enable-pop --enable-mailtool  --with-ssl
+    ./configure --enable-imap --enable-smtp --enable-pop --enable-mailtool  --with-ssl --enable-hcache  --with-sasl=/usr/local
     make
     sudo make install
     mkdir ~/.mutt
